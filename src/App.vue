5c788d7ce93b19e67560de6d87def7522a1be3bd
prettyntakirutimana <prettyntakirutimana@gmail.com>
2020-11-30 09:25:13 +0200

    This is the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex">
+      <Banner />
+      <Login />
+    </div>
+
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Banner from "./components/Banner.vue";
+import Footer from "./components/Footer.vue";
+import Login from "./components/Login.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Banner,
+    Footer,
+    Login,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./assets/tailwind.css">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap");
 </style>
