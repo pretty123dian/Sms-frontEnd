cb703e39bd042325e41d0b54dc62f59b881b425d
ishgervais <ishimwegervais11@gmail.com>
2020-12-01 08:03:42 +0200

    Intro to search bar

---
+++ src/components/shared/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="search-bar">
+<input type="search" name="" id="">
+  </div>
+</template>
+
+<script>
+export default {
+name:"SearchBar"
+}
+</script>
+
+<style>
+.search-bar{
+    border: 1px solid black;
+    height: 2vh ;
+}
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="grid grid-cols-12 gap-2 dashboard">
       <Sidebar/>
+      <!-- <search-bar/> -->
       <statistical-visual/>
       <right-sidebar/>
   </div>
@@ -10,12 +11,14 @@
 import Sidebar from '../components/shared/Sidebar';
 import RightSidebar from '../components/shared/RightSidebar';
 import StatisticalVisual from '../components/main/StatisticalVisual.vue';
+// import SearchBar from '../components/shared/SearchBar.vue';
 export default {
 name: "Dashboard",
 components:{
 Sidebar,
 RightSidebar,
-StatisticalVisual
+StatisticalVisual,
+// SearchBar
     
 }
 }
