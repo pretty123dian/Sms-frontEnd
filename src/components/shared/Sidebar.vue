977b80bd40ccd4b405b4aace4874cb16af615ece
ishgervais <ishimwegervais11@gmail.com>
2021-01-02 15:37:25 +0200

    rechanging scrol bar

--- src/components/shared/Sidebar.vue
+++ src/components/shared/Sidebar.vue
@@ -209,6 +209,38 @@
 </svg>
 Timetable
       </router-link>
+          <router-link to="/view/timetable">
+        
+<svg width="18" height="18"   class="svg__timetable"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path class="svg__path"  d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path class="svg__path" d="M12 6V12L16 14" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+Timetable
+      </router-link>
+          <router-link to="/view/timetable">
+        
+<svg width="18" height="18"   class="svg__timetable"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path class="svg__path"  d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path class="svg__path" d="M12 6V12L16 14" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+Timetable
+      </router-link>
+          <router-link to="/view/timetable">
+        
+<svg width="18" height="18"   class="svg__timetable"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path class="svg__path"  d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path class="svg__path" d="M12 6V12L16 14" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+Timetable
+      </router-link>
+          <router-link to="/view/timetable">
+        
+<svg width="18" height="18"   class="svg__timetable"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path class="svg__path"  d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path class="svg__path" d="M12 6V12L16 14" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+Timetable
+      </router-link>
 
       <router-link to="/">
         <svg
@@ -237,6 +269,18 @@ export default {
 };
 </script>
 <style>
+.sidebar::-webkit-scrollbar{
+  width: 5px;
+}
+.sidebar::-webkit-scrollbar-track        { 
+   background-color: #e3e1e9c2; 
+   width: 1%;
+ }
+.sidebar::-webkit-scrollbar-thumb        {
+  background-color: #b9b9bb63;
+     
+     border-radius: 40px;
+   }
 .sidebar {
   /* border:1px solid black; */
   height: 98vh;
