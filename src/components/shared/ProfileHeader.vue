26af4ff7cda1bc210b515fa9a254bf6f8a18959a
ishgervais <ishimwegervais11@gmail.com>
2021-01-04 18:21:47 +0200

    adding a dropdown on avatar png

--- src/components/shared/ProfileHeader.vue
+++ src/components/shared/ProfileHeader.vue
@@ -1,6 +1,12 @@
 <template>
-  <div :class="[this.$route.name=='Dashboard'?'grid grid-cols-2 gap-2 mt-3':'grid grid-cols-2 gap-2']">
-    <div class="md:col-start-8 md:col-end-10 ">
+  <div
+    :class="[
+      this.$route.name == 'Dashboard'
+        ? 'grid grid-cols-2 gap-2 mt-3'
+        : 'grid grid-cols-2 gap-2',
+    ]"
+  >
+    <div class="md:col-start-8 md:col-end-10">
       <div class="relative w-10 h-10 mt-3">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -12,7 +18,6 @@
           <path fill="none" d="M0 0h24v24H0z" />
           <path
             d="M22 20H2v-2h1v-6.969C3 6.043 7.03 2 12 2s9 4.043 9 9.031V18h1v2zM5 18h14v-6.969C19 7.148 15.866 4 12 4s-7 3.148-7 7.031V18zm4.5 3h5a2.5 2.5 0 1 1-5 0z"
-           
           />
         </svg>
         <div
@@ -20,93 +25,99 @@
         ></div>
       </div>
     </div>
-    <div class="col-start-10 col-end-13 md:pr-10">
-    <div class="grid grid-cols-2 gap-2 md:pr-10 cursor-pointer border">
-      <div class="col-start-1 col-end-3">
-        <img
-          class="rounded-full border border-gray-100 shadow-sm w-10 h-10"
-          src="https://randomuser.me/api/portraits/women/81.jpg"
-          alt="user image"
-           @click="setDropdown"
-        />
+
+
+
+
+
+    <div class="col-start-10 col-end-13 grid grid-cols-2 gap-2 md:pr-10">
+      <div class="col-start-1 col-end-3 flex">
+        <div class="col-start-1 col-end-3 px-2 cursor-pointer">
+          <img
+            class="rounded-full border border-gray-100 shadow-sm w-10 h-10"
+            src="https://randomuser.me/api/portraits/women/81.jpg"
+            alt="user image"
+            @click="setDropdown"
+          />
+        </div>
+        <div class="col-start-3 col-end-13 cursor-pointer" @click="setDropdown">
+          <b>Admin Code Lento</b> <br />
+          <span class="text-xs">Admin</span>
+        </div>
       </div>
-      <div class="col-start-3 col-end-13" @click="setDropdown">
-        <b>Admin Code Lento</b> <br />
-        <span class="text-xs">Admin</span>
+      <div
+        v-if="addDropdown === true"
+        class="card bg-white p-2 lg:w-1/6 md:w-full rounded user__preview_more shadow"
+      >
+        <ul class="lg:px-1 lg:py-2">
+          <template v-for="(link, index) in dropdownStore">
+            <li :key="index">
+              <router-link :to="link.link">{{ link.name }}</router-link>
+            </li>
+          </template>
+        </ul>
       </div>
-
-      
-
-
     </div>
-     <div v-if="addDropdown===true" class="card bg-white p-2 lg:w-1/6 md:w-full rounded user__preview_more shadow">
-       <ul class="lg:px-2 lg:py-2">
-         <template v-for="(link,index) in dropdownStore">
-            <li :key="index"><router-link :to="link.link">{{link.name}}</router-link></li>
-         </template>
-       </ul>
-         </div>
-         </div>
+
+
+
+
+
   </div>
 </template>
 
 <script>
 export default {
   name: "ProfileHeader",
-  data:()=>({
-    addDropdown:false,
-    drop_icon:'+',
+  data: () => ({
+    addDropdown: false,
+    drop_icon: "+",
     dropdownStore: [
       {
-        name:'Profile settings',
-        link:'/dashboard'
+        name: "Profile settings",
+        link: "/dashboard",
       },
       {
-        name:'Logout',
-        link:'/'
+        name: "Logout",
+        link: "/",
       },
-        
-     
-    ]
+    ],
   }),
-  computed(){
-     this.addDropdown= false
+  computed() {
+    this.addDropdown = false;
   },
-  methods:{
-    setDropdown(){
-      if(this.drop_icon !='-'){
-        this.drop_icon = '-'
-      }
-      else
-        this.drop_icon = '+'
-       
-      
-      return this.addDropdown = !this.addDropdown;
-    }
-  }
+  methods: {
+    setDropdown() {
+      if (this.drop_icon != "-") {
+        this.drop_icon = "-";
+      } else this.drop_icon = "+";
+
+      return (this.addDropdown = !this.addDropdown);
+    },
+  },
 };
 </script>
 
 <style scoped>
-.user__preview_more{
-    position: absolute;
-    margin-top: 0.5%;
-    align-items: center;
-    z-index: 500;
-    width: 15%;
+.user__preview_more {
+  position: absolute;
+  margin-top: 3%;
+  align-items: center;
+  z-index: 500;
+  width: 13%;
 }
-ul li{
+ul li {
   padding: 3%;
   font-size: 13px;
 }
-ul li:hover{
-  color: #574AE2;
+ul li:hover {
+  color: #574ae2;
 }
-.bell-notify{
-    fill: #000;
+.bell-notify {
+  fill: #000;
 }
-.bell-notify:hover{
-fill: #574AE2;
-cursor: pointer;
+.bell-notify:hover {
+  fill: #574ae2;
+  cursor: pointer;
 }
 </style>
