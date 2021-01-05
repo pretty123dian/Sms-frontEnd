ed821455a2d6e98f0c744a677589f2a363c6ad24
ishgervais <ishimwegervais11@gmail.com>
2021-01-05 22:34:57 +0200

    adding labels to the form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,9 +6,11 @@
         {{ login_error }} <svg class="cursor-pointer" @click="isIncorrect=false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-11.414L9.172 7.757 7.757 9.172 10.586 12l-2.829 2.828 1.415 1.415L12 13.414l2.828 2.829 1.415-1.415L13.414 12l2.829-2.828-1.415-1.415L12 10.586z" fill="rgba(255,156,148,1)"/></svg>
       </div>
       <div :class="[!isEmptyPassword && !isEmpty ? 'mt-5' : 'mt-10']">
-        <p :class="[isEmpty == true ? 'mb-4' : 'mb-4 label_error']">
+      <div class="flex">
+        <label :class="[isEmpty == true ? 'mb-4' : 'mb-4 label_error']">
           {{ username_title }}
-        </p>
+        </label>
+        </div>
 
         <input
           type="text"
@@ -24,9 +26,9 @@
 
       <div class="mt-10">
         <div class="flex">
-          <p :class="[isEmptyPassword == true ? '' : 'label_error']">
+          <label :class="[isEmptyPassword == true ? '' : 'label_error']">
             {{ password_title }}
-          </p>
+          </label>
         </div>
 
         <br />
