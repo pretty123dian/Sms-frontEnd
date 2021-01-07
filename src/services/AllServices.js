0b64640b3b1e20334887fa5a4b7001bde6adb290
ishgervais <ishimwegervais11@gmail.com>
2021-01-07 09:04:24 +0200

    commenting all services and general hint

--- src/services/AllServices.js
+++ src/services/AllServices.js
@@ -1,55 +1,58 @@
 import Api from './api';
-export default{
-    getTimeTable(){
+export default {
+    getTimeTable() {
         return Api().get(`api/time-table`);
     },
 
-    getPromotion(promotion_id){
+    getPromotion(promotion_id) {
         return Api().get(`api/school-promotions/${promotion_id}`);
     },
 
-    getSemesters(n_of_pages, pages_limit){
+    getSemesters(n_of_pages, pages_limit) {
         return Api().get(`api/semester?page=${n_of_pages}&limit=${pages_limit}`);
     },
-    getClassPromotion(){
+    getClassPromotion() {
         return Api().get(`api/school-promotion-classes`);
     },
-    getClasses(){
+    getClasses() {
         return Api().get(`api/school-classes`);
     },
-    getCategories(){
+    getCategories() {
         return Api().get(`api/user-categories`);
     },
-    getCategory(passcode){
+    getCategory(passcode) {
         return Api().get(`api/user-categories/${passcode}`);
     },
-    getNotes(){
+    getNotes() {
         return Api().get(`api/course-upload`);
     },
-    getAssignments(n_of_pages,pages_limit){
+    getAssignments(n_of_pages, pages_limit) {
         return Api().get(`api/assignment?page=${n_of_pages}&limit=${pages_limit}`)
     },
-    getLessons(){
+    getLessons() {
         return Api().get(`api/lessons`);
     },
 
-    getLessonPromotionById(promotion_id){
+    getLessonPromotionById(promotion_id) {
         return Api().get(`api/lessons-promotion/${promotion_id}`);
     },
-    getLessonById(lesson_id){
+    getLessonById(lesson_id) {
         return Api().get(`api/lessons/${lesson_id}`)
     },
-    getUsers(){
+    getUsers() {
         return Api().get(`api/users`);
     },
-    getUserData(user_id){
+    getUserData(user_id) {
         return Api().get(`api/users/${user_id}`)
     },
 
 
+    // GETTING APIS END HERE
+
+    // POSTING APIS START HERE
 
 
-    login(body){
-        return Api().post(`api/users/auth`,body)
+    login(body) {
+        return Api().post(`api/users/auth`, body)
     }
 }
