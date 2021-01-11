be49df5b489c6c8eaf4dbf9fbbc1b98291769d71
pretty123dian <prettyntakirutimana@gmail.com>
2021-01-11 18:33:33 +0200

    new commit

--- src/components/lessons/LessonsTable.vue
+++ src/components/lessons/LessonsTable.vue
@@ -48,8 +48,13 @@
       </div>
       <div class="center bg-white p-5">
         <table stripe :data="lessons" class="w-full">
-            <input class="form-input p-2 border rounded" v-model="search" border placeholder="Search lesson" />
-        
+          <input
+            class="form-input p-2 border rounded"
+            v-model="search"
+            border
+            placeholder="Search lesson"
+          />
+
           <template class="bg-white">
             <transition name="fade" appear>
               <div
@@ -67,25 +72,10 @@
             </tr>
           </template>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
           <template>
-            <tr :key="i" v-for="(tr, i) in lessons" :data="tr">
-              <td>
-                {{ tr.id }}
-=======
-=======
-
->>>>>>> 3b2d69fb982f6a1b15f4ad2144bffe1689b1de07
-          <template >
             <tr :key="i" v-for="(tr, i) in searchSimilar" :data="tr">
               <td class="w-1/6">
-                {{i+1}}
-<<<<<<< HEAD
->>>>>>> 9998d5a01104acdce016ebcc975f98bda9944526
-=======
-
->>>>>>> 3b2d69fb982f6a1b15f4ad2144bffe1689b1de07
+                {{ i + 1 }}
               </td>
               <td>
                 {{ tr.name }}
@@ -105,35 +95,16 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-import AddLesson from "./AddLesson";
-=======
-import Services from '@/services/AllServices'
->>>>>>> 9998d5a01104acdce016ebcc975f98bda9944526
-=======
-import Services from '@/services/AllServices'
+import Services from "@/services/AllServices";
 
 import AddLesson from "./AddLesson";
 
->>>>>>> 3b2d69fb982f6a1b15f4ad2144bffe1689b1de07
 export default {
   name: "LessonsTable",
-  methods: {
-    doIt() {
-      this.showModal = false;
-      console.log("yhyh");
-    },
-  },
   components: {
     AddLesson,
   },
   data: () => ({
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 3b2d69fb982f6a1b15f4ad2144bffe1689b1de07
     showModal: false,
     lessons: [
       {
@@ -142,60 +113,60 @@ export default {
         description:
           "This is the course of mathematics which deals with analysis,...",
         action: [
-<<<<<<< HEAD
-=======
-    action: [
->>>>>>> 9998d5a01104acdce016ebcc975f98bda9944526
-=======
->>>>>>> 3b2d69fb982f6a1b15f4ad2144bffe1689b1de07
           `<svg  xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 26.998 27">
-  <path id="Icon_material-update" data-name="Icon material-update" d="M31.5,15.18H21.33l4.11-4.23a10.558,10.558,0,0,0-14.82-.15,10.313,10.313,0,0,0,0,14.685,10.529,10.529,0,0,0,14.82,0A9.77,9.77,0,0,0,28.5,18.15h3a14.011,14.011,0,0,1-3.96,9.435,13.581,13.581,0,0,1-19.08,0,13.26,13.26,0,0,1-.03-18.87,13.481,13.481,0,0,1,18.975,0L31.5,4.5ZM18.75,12v6.375L24,21.495,22.92,23.31,16.5,19.5V12Z" transform="translate(-4.502 -4.5)" fill="#574ae2"/>
-</svg>`,
+            <path id="Icon_material-update" data-name="Icon material-update" d="M31.5,15.18H21.33l4.11-4.23a10.558,10.558,0,0,0-14.82-.15,10.313,10.313,0,0,0,0,14.685,10.529,10.529,0,0,0,14.82,0A9.77,9.77,0,0,0,28.5,18.15h3a14.011,14.011,0,0,1-3.96,9.435,13.581,13.581,0,0,1-19.08,0,13.26,13.26,0,0,1-.03-18.87,13.481,13.481,0,0,1,18.975,0L31.5,4.5ZM18.75,12v6.375L24,21.495,22.92,23.31,16.5,19.5V12Z" transform="translate(-4.502 -4.5)" fill="#574ae2"/>
+          </svg>`,
           `<svg class="delete-bin-line" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 27 27">
-  <path id="Path_473" data-name="Path 473" d="M0,0H27V27H0Z" fill="none"/>
-  <path id="Path_474" data-name="Path 474" d="M19,6.533h5.667V8.8H22.4V23.533a1.133,1.133,0,0,1-1.133,1.133H5.4a1.133,1.133,0,0,1-1.133-1.133V8.8H2V6.533H7.667v-3.4A1.133,1.133,0,0,1,8.8,2h9.067A1.133,1.133,0,0,1,19,3.133ZM20.133,8.8H6.533V22.4h13.6Zm-10.2,3.4H12.2V19H9.933Zm4.533,0h2.267V19H14.467ZM9.933,4.267V6.533h6.8V4.267Z" transform="translate(0.167 0.167)" fill="#db222a"/>
-</svg>`,
+            <path id="Path_473" data-name="Path 473" d="M0,0H27V27H0Z" fill="none"/>
+            <path id="Path_474" data-name="Path 474" d="M19,6.533h5.667V8.8H22.4V23.533a1.133,1.133,0,0,1-1.133,1.133H5.4a1.133,1.133,0,0,1-1.133-1.133V8.8H2V6.533H7.667v-3.4A1.133,1.133,0,0,1,8.8,2h9.067A1.133,1.133,0,0,1,19,3.133ZM20.133,8.8H6.533V22.4h13.6Zm-10.2,3.4H12.2V19H9.933Zm4.533,0h2.267V19H14.467ZM9.933,4.267V6.533h6.8V4.267Z" transform="translate(0.167 0.167)" fill="#db222a"/>
+          </svg>`,
           `
-<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 36 27">
-  <g id="Icon_feather-eye" data-name="Icon feather-eye" transform="translate(0 -4.5)">
-    <path id="Path_475" data-name="Path 475" d="M1.5,18S7.5,6,18,6,34.5,18,34.5,18,28.5,30,18,30,1.5,18,1.5,18Z" fill="none" stroke="#2bde73" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-    <path id="Path_476" data-name="Path 476" d="M22.5,18A4.5,4.5,0,1,1,18,13.5,4.5,4.5,0,0,1,22.5,18Z" fill="none" stroke="#2bde73" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-  </g>
-</svg>
-`,
+          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 36 27">
+            <g id="Icon_feather-eye" data-name="Icon feather-eye" transform="translate(0 -4.5)">
+              <path id="Path_475" data-name="Path 475" d="M1.5,18S7.5,6,18,6,34.5,18,34.5,18,28.5,30,18,30,1.5,18,1.5,18Z" fill="none" stroke="#2bde73" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+              <path id="Path_476" data-name="Path 476" d="M22.5,18A4.5,4.5,0,1,1,18,13.5,4.5,4.5,0,0,1,22.5,18Z" fill="none" stroke="#2bde73" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+            </g>
+          </svg>
+          `,
         ],
-    search:'',
-    lessons: [],
+        search: "",
+        lessons: [],
+      },
+    ],
   }),
-    computed:{
-    searchSimilar(){
-       let filter = new RegExp(this.search,'i');
-       let foundText = this.lessons.filter(el=>el.name.match(filter))
-          return foundText;    
-    }
+  computed: {
+    searchSimilar() {
+      let filter = new RegExp(this.search, "i");
+      let foundText = this.lessons.filter((el) => el.name.match(filter));
+      return foundText;
+    },
   },
 
-  beforeMount(){
-this.lessons = [];
-this.getRows();
+  beforeMount() {
+    this.lessons = [];
+    this.getRows();
   },
- 
-  methods:{
-    async getRows(){
-        const response = await Services.getLessons();
-        console.log("Response: ", response);
-        response.data.data.docs.forEach(lesson=>{
-         const lessonObj = {};
-         lessonObj.name = lesson.name;
-         lessonObj.description = lesson.description;
-         lessonObj.status = lesson.status;
-         lessonObj.action = this.action;
 
-         this.lessons.push(lessonObj);
-        })
-        console.log("Lessons: ", this.lessons);
-    } 
-  }
+  methods: {
+    doIt() {
+      this.showModal = false;
+      console.log("yhyh");
+    },
+    async getRows() {
+      const response = await Services.getLessons();
+      console.log("Response: ", response);
+      response.data.data.docs.forEach((lesson) => {
+        const lessonObj = {};
+        lessonObj.name = lesson.name;
+        lessonObj.description = lesson.description;
+        lessonObj.status = lesson.status;
+        lessonObj.action = this.action;
+
+        this.lessons.push(lessonObj);
+      });
+      console.log("Lessons: ", this.lessons);
+    },
+  },
 };
 </script>
 
@@ -221,8 +192,10 @@ label > input[type="checkbox"]:checked + * {
 tr {
   border-bottom: 1px solid #dfdfdf !important;
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+input:focus {
+  /* border: 1px solid #574ae2; */
+}
 svg:hover {
   cursor: pointer;
   fill: indigo;
@@ -231,26 +204,6 @@ svg:hover {
 .popup {
   z-index: 1;
   background-color: rgba(0, 0, 0, 0.3);
-=======
-=======
-
->>>>>>> 3b2d69fb982f6a1b15f4ad2144bffe1689b1de07
-input:focus{
- /* border: 1px solid #574ae2; */
-}
-svg:hover{
-  cursor:pointer;
-  fill:indigo
-<<<<<<< HEAD
->>>>>>> 9998d5a01104acdce016ebcc975f98bda9944526
-=======
-}
-
-.popup {
-  z-index: 1;
-  background-color: rgba(0, 0, 0, 0.3);
-
->>>>>>> 3b2d69fb982f6a1b15f4ad2144bffe1689b1de07
 }
 </style>
 
