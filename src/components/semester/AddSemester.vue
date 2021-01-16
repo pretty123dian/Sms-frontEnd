24c443d83338443a1a14471e6b4ac0987b7d1020
ishgervais <ishimwegervais11@gmail.com>
2021-01-16 13:37:31 +0200

    removed spaces

--- src/components/semester/AddSemester.vue
+++ src/components/semester/AddSemester.vue
@@ -19,7 +19,7 @@
               @blur="labels_focus.label1 = !labels_focus.label1"
               v-model="name"
               id="name"
-              class="form-input p-3 mt-2 mr-4 border lg:w-3/4 md:w-full border-#E1E1E1-600 rounded"
+              class="form-input p-3 mt-2 mr-4 border lg:w-4/4 md:w-full border-#E1E1E1-600 rounded"
             />
           </div>
           <div class="col-span-5 lg:col-span-1 md:col-span-5 mt-3">
@@ -35,7 +35,7 @@
               @blur="labels_focus.label2 = !labels_focus.label2"
               v-model="year"
               id="name"
-              class="form-input p-3 mt-2 mr-4 border lg:w-3/4 md:w-full border-#E1E1E1-600 rounded"
+              class="form-input p-3 mt-2 mr-4 border lg:w-4/4 md:w-full border-#E1E1E1-600 rounded"
             >
               <option value="">Select academic year</option>
               <template v-for="(year, index) in academicYears">
@@ -43,31 +43,8 @@
               </template>
             </select>
           </div>
-          <!-- <div class="col-span-5 lg:col-span-1 md:col-span-5 mt-4">
-            <label
-              for=""
-              class="mr-4"
-              :class="[labels_focus.label2 ? 'label-focus' : '']"
-              >Description</label
-            >
-            <br />
-            <textarea
-              @focus="labels_focus.label2 = !labels_focus.label2"
-              @blur="labels_focus.label2 = !labels_focus.label2"
-              v-model="description"
-              id=""
-              rows="5"
-              class="form-input p-3 mt-2 mr-4 border lg:w-3/4 sm:w-full border-#E1E1E1-600 rounded"
-            >
-            </textarea>
-          </div> -->
 
           <div class="col-span-5 lg:col-span-1 md:col-span-5 mt-12">
-            <!-- <vs-button
-              color="#574AE2"
-              class="focus:outline-none lg:w-2/4 sm:w-full"
-              >Add semester</vs-button -->
-
             <button
               @click="validateAllfields()"
               :class="[
