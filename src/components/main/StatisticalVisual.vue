b2fca827b465dcefeac30e9ad878b66641dbbbae
ishgervais <ishimwegervais11@gmail.com>
2021-02-08 09:18:59 +0200

    Added the vue chart js and ready to do analysis

--- src/components/main/StatisticalVisual.vue
+++ src/components/main/StatisticalVisual.vue
@@ -1,36 +1,98 @@
 <template>
 <div>
         <div class="chart-one lg:ml-4 sm:ml-4">
-
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
         </div>
         
         <div class="chart-two lg:ml-4 sm:ml-4">
-            
+             <line-chart :chart-data="datacollection"></line-chart>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-name:"StatisticalVisual"
-}
+// export default {
+// name:"StatisticalVisual"
+// }
+ import LineChart from './LineChart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+        //   labels: [this.getRandomInt(), this.getRandomInt()],
+        //   datasets: [
+        //     {
+        //       label: 'Data One',
+        //       backgroundColor: '#574ae2',
+        //       data: [this.getRandomInt(), this.getRandomInt()]
+        //     }, {
+        //       label: 'Data One',
+        //       backgroundColor: '#4caf50',
+        //       data: [this.getRandomInt(), this.getRandomInt()]
+        //     }
+            
+        //   ]
+           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#574ae2',
+          data: [40, 39, 10, 40, 39, 80, 40]
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false}
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    
+  }
 </script>
 
 <style>
+
+
 .chart-one{
+    padding: 10px;
     /* border:1px solid black; */
-    height: 50vh;
+    min-height: 550px;
     background-color: #fff;
     /* border:1px solid rgb(172, 169, 169); */
     border-radius: 5px;
+    position: relative;
+}
+#line-chart{
+    height: 450px !important;
+    width:90% !important;
+    margin: 5%;
+    position: relative !important;
 }
 .chart-two{
     margin-top: 5%;
-    /* border:1px solid rgb(172, 169, 169); */
-    height: 50vh;
-    border-radius: 5px;
     margin-bottom: 10%;
+      padding: 10px;
+    /* border:1px solid black; */
+    min-height: 550px;
     background-color: #fff;
+    /* border:1px solid rgb(172, 169, 169); */
+    border-radius: 5px;
+    position: relative;
 }
 
 </style>
