6f09595f96d00b83f85f2f6f5adbf4661c5eb9a1
ishgervais <ishimwegervais11@gmail.com>
2021-02-12 11:00:47 +0200

    reformatted the instructor form

--- src/components/instructors/AddInstructor.vue
+++ src/components/instructors/AddInstructor.vue
@@ -164,7 +164,7 @@
         <button
           :class="[
             request_click === true
-              ? 'button_on_loading border rounded-md py-2 px-5 text-white add__btn flex  lg:w-6/6 sm:w-1/4'
+              ? 'button_on_loading border rounded-md py-3 px-5 text-white add__btn flex  lg:w-6/6 sm:w-1/4'
               : 'border rounded-md py-3 px-5  text-white  add__btn flex  lg:w-full sm:w-1/4',
           ]"
         >
@@ -174,7 +174,7 @@
           <template v-else>
             <!-- <img src="@/assets/gif2.gif" width="40" /> -->
             <!-- Registering ... -->
-              <ClipLoader 
+              <ClipLoader   size="27px"
              color="white"/>
           </template>
         </button>
